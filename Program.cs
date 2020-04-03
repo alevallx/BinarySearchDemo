3da83aa5a1fc994cd367d067a39107423e0d25c7
alevall0 <37554522+alevall0@users.noreply.github.com>
2020-04-02 22:42:03 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace BuscarNroEnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Posicion :"  + RetornaIndice(65));
+        }
+
+        static int RetornaIndice(int nroABuscar)
+        {
+
+            int[] arrNros = new int[1000];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                arrNros[i] = i;
+            }
+
+            return BinarySearch(arrNros,nroABuscar);
+
+        }
+
+        static int BinarySearch(int[] arr , int val)
+        {
+            int ini = 0;
+            int end = arr.Length - 1;
+
+            while (ini <= end)
+            {
+                int mid = (ini + end) / 2;
+                if (arr[mid] == val)
+                {
+                    return mid;
+                }
+                else if (val > arr[mid])
+                {
+                    ini = mid + 1;
+                }
+                else
+                    end = mid - 1;
+
+            }
+            return -1;
+        }
+    }
+}
